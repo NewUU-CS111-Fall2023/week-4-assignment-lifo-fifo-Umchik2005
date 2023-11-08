ca19cb1c4c987f477ebf39d94cf7b01044fd9d25
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2023-11-08 21:17:15 +0500

    updating task5

--- src/task_5.h
+++ src/task_5.h
@@ -13,25 +13,3 @@ class FifthTask {
 
 
 
-// void fifthTask(){
-//     int numberOfTowers(int num);
-//     int n = 8;
-//     int minNumMoves = numberOfTowers(n);
-    
-
-//     int numberOfTowers(int num){
-//          if (num == 1){ 
-//             return 1; // T(1) = 1
-//         } else {
-//             return 2 * numberOfTowers(num-1) + 1;
-//         }
-//     }
-    
-//     std::cout << minNumMoves << "\n";
-// }
-
-
-
-
-
-
